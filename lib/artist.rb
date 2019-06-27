6a29e74d2ce49527b9b5955b47b58ef5935a2db4
Ursula Miller <ursula.miller@gmail.com>
2019-06-27 16:29:58 +0000

    Automatically backed up by Learn

---
+++ lib/artist.rb
@@ -0,0 +1,11 @@
+class Artist 
+ def initialize(name) 
+   @name = name
+   @songs = []
+ end
+ 
+ attr_accessor :name
+ 
+ 
+ 
+end
---
+++ lib/song.rb
@@ -0,0 +1,5 @@
+class Song 
+  def initialize(song_title)
+    @title = song_title
+  end
+end
